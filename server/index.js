a7d258542b26832bc993ee855e4264300bf8a116
ARYANNJ <91716193+ARYANNJ@users.noreply.github.com>
2023-08-14 03:12:48 +0530

    Delete server directory

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
-const app=express();
-
-mongoose.connect("mongodb+srv://AryanJ:%40shokNEHA123@merntodo.8hyddy1.mongodb.net/?retryWrites=true&w=majority")
-.then(()=>app.listen(8000))
-.then(()=>console.log("Connected to mongoDB"))
-.catch((err)=>console.log(err));
